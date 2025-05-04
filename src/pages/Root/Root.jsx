8793bb806b0d1f0a6c4da2098e0cf9ce89f78cb4
fazlulkarimyousuf <faizmiah52@gmail.com>
2025-05-04 10:36:03 +0600

    add all file ....

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,20 +1,7 @@
-// import React from 'react';
-// import NAvbar from '../../components/Header/NAvbar';
-// import {Outlet} from 'react-router';
-// import Footer from '../../components/Footer/Footer';
-
-// const Root = () => {
-//     return (
-//         <div>
-//             <NAvbar></NAvbar>
-//             <Outlet></Outlet>
-//             <Footer></Footer>
-//         </div>
-//     );
-// };
-
-// export default Root;
 import React from 'react';
+import {Outlet} from 'react-router';
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Header/Navbar/NAvbar';
 
 const Root = () => {
     return (
