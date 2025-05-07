94084f6285c7e7cbfcbb96c12ae0c693e681e684
fazlulkarimyousuf <faizmiah52@gmail.com>
2025-05-07 14:53:54 +0600

    add file ........

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import bookimage from '../../assets/books.jpg' 
 
 const Banner = () => {
     return (
-        <div>
-            <div>
-                <h1>Books to freshen up your bookshelf</h1>
-                <button className='btn bg-green-700 text-white'>View The List</button>
-            </div>
-            <div>
-                <img src=""alt="" />
-            </div>
-        </div>
+         <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src={bookimage}className="max-w-sm rounded-lg shadow-2xl"/>
+    <div>
+      <h1 className="text-5xl font-bold">Books to freshen up 
+         <br /> your bookshelf</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button className="btn bg-green-600">View The List</button>
+    </div>
+  </div>
+</div>
     );
 };
 
